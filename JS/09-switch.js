1d959c5c5f49ea10f1cf88b2b607f4ea3debbd1e
Koretto777 <182255233+koretto777@users.noreply.github.com.>
2025-09-30 17:51:44 -0600

    fundamentos de js - sesión 3

---
+++ JS/09-switch.js
@@ -0,0 +1,36 @@
+/* 
+
+switch (variable) {
+// Casos según se comparen
+
+case valor1:
+  // El codigo a ejecutar
+  brak;
+case valor2:
+  // El código a ejecutar
+  break;
+case valor3:
+  // El código a ejecutar
+  break;
+default:
+  // Este es el código que se ejecuta si la exposión no coincide con ningun caso.
+  break; // Esto es opcional  
+  }
+
+*/
+
+// Variable a evaluar
+let licenciatura = "televisión"; // televisión, diseño, colsultoría
+
+switch (licenciatura) {
+  case "televisión":
+    console.log("Haz elegido la licenciatura de Televisión, que mal por ti!");
+  case "diseño":
+    console.log("Haz elegido la licenciatura de Diseño, serás muy fregón!")
+  case "imagen":
+    console.log("Haz elegido la licenciatura de Imagen que bien por ti!")
+  default:
+    console.log("Estas loquitx")
+  
+}
+
